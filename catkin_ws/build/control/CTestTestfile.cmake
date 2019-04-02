55e851e8a8f4da80de96d7ace46552d49b299caf
zeryab.moussaoui <zeryab.moussaoui@gmail.com>
2019-04-02 21:43:51 +0200

    Delete build directory

--- catkin_ws/build/control/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/houssem-meg/submission/catkin_ws/src/control
-# Build directory: /home/houssem-meg/submission/catkin_ws/build/control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
